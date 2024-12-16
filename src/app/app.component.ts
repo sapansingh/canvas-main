9cc3cafd77d75be7a2d05f14e133628b0d787699
sapansingh <sapansinghchouhan@gmail.com>
2024-12-16 13:32:34 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {ChangeDetectionStrategy, signal} from '@angular/core';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [MatExpansionModule,MatIconModule,MatToolbarModule, MatButtonModule, MatIconModule,MatMenuModule,MatSidenavModule,MatListModule,RouterOutlet,CommonModule,MatTooltipModule ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+ 
+})
+export class AppComponent {
+  title = 'canvas';
+  showFiller = false;
+  isSubMenuVisible = false;
+  readonly panelOpenState = signal(false);
+  toggleSubMenu() {
+    this.isSubMenuVisible = !this.isSubMenuVisible;
+  }
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { AssetsdetailsComponent } from './components/assetsdetails/assetsdetails.component';
+
+export const routes: Routes = [
+
+    {
+        path:"table",
+        component:AssetsdetailsComponent
+    }
+   
+];
