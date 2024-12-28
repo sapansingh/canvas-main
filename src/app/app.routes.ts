b82c2ae773a2fbe08722066df8a25034bf460233
sapansingh <sapansinghchouhan@gmail.com>
2024-12-28 19:50:48 +0530

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AssetsdetailsComponent } from './components/inventory/assetsdetails/assetsdetails.component';
 import { AddinventoryComponent } from './components/inventory/addinventory/addinventory.component';
 import { InventoryissuereceivedComponent } from './components/inventory/inventoryissuereceived/inventoryissuereceived.component';
+import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
 
 export const routes: Routes = [
 
@@ -17,6 +18,9 @@ export const routes: Routes = [
     {
         path:"inventoryissue",
         component:InventoryissuereceivedComponent
+    },
+    {path:"dashboard",
+        component:DashboardComponent
     }
    
 ];
